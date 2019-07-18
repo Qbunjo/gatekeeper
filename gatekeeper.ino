6c4b4db83c67de787fd739dbdc761876d8f19d14
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-07-18 14:50:20 +0200

    Update gatekeeper.ino

--- gatekeeper.ino
+++ gatekeeper.ino
@@ -33,7 +33,7 @@ void loop() {
   if (curGateSt == 1 && curGaragSt == 1){
     evClosed=1;}// if everything is closed, flag on
   else {evClosed=0;}
-  if (evClosed=1) {
+  if (evClosed==1) {
     timer++;}//if flag is on, count to lcd turnoff
   if (curGaragSt ==1 && prevGaragSt ==0) {
     timer=0;} //if garage was closed and now opens, reset timer
