bf731a62eaa4e51623e8412808154ed9e5f16219
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-01-29 11:10:50 +0100

    i2c with screensaver

--- gatekeeper.ino
+++ gatekeeper.ino
@@ -5,6 +5,7 @@ int prevGateSt = 0;// gate memory variable
   int timer =0; //timer
 int evClosed = 0; //all closed flag
 // include the library code:
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 // initialize the library with the numbers of the interface pins
@@ -12,7 +13,7 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
      // lcd settings
-  lcd.begin(16, 2);
+  lcd.init();
   TurnOn();
   
   // boot up
@@ -20,12 +21,12 @@ void setup() {
   pinMode (7, INPUT);
   pinMode (10, OUTPUT);
   digitalWrite(10,HIGH);
-  Serial.begin(9600);
+//  Serial.begin(9600);
   //prepare lcd
   lcd.setCursor(0,0);
-  lcd.print(" GATE  ");
+  lcd.print(" BRAMA  ");
   lcd.setCursor(0,1);
-  lcd.print(" GARAGE");
+  lcd.print(" GARAZ");
 }
 void loop() {
   pinMode (8, INPUT);
@@ -45,8 +46,8 @@ void loop() {
     timer=0;} //if garage opens, reset timer
   if (curGateSt ==0) {
     timer=0;} //if gate opens, reset timer
-  Serial.println(timer);//show timer in serial
-  if (timer >=25){
+ // Serial.println(timer);//show timer in serial
+  if (timer >=500){
     TurnOff();
   }//time to lcd turnoff
   if (curGateSt==0 && prevGateSt==1){
@@ -57,28 +58,28 @@ void loop() {
   if (curGateSt == LOW)
   {
   lcd.setCursor(7, 0);
-  lcd.print("  OPEN  ");
+  lcd.print(" OTWARTA  ");
      }
   else
         lcd.setCursor(7, 0);
-    lcd.print("  CLOSED");
+    lcd.print("ZAMKNIETA");
   
   //print in the second row
   if (curGaragSt == LOW)
   {
   lcd.setCursor(7,1);
-  lcd.print("  OPEN    ");
+  lcd.print(" OTWARTY    ");
      }
   else
     lcd.setCursor(7, 1);
-    lcd.print("  CLOSED");
+    lcd.print("ZAMKNIETY");
   if (curGateSt == LOW || curGaragSt == LOW);
   {
    digitalWrite(10,HIGH);
   }
     prevGateSt=curGateSt; //remember previous state
     prevGaragSt=curGaragSt;
-  if (timer==500){
+  if (timer==2000){
     timer=1;}//do not count to infinity
 }
 void TurnOn(){
